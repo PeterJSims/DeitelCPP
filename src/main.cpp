e353ec8c587dc3c1cc5b8795181d57fef6cfa580
PeterJSims <ragtimesims@gmail.com>
2024-11-02 09:49:59 -0400

    Clearing out certain files

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,5 @@
-// #include <algorithm>
-// #include <iostream>
-// #include <concepts>
-// #include <type_traits>
-// #include <list>
-//
-// template <typename T>
-// concept Numeric = std::integral<T> || std::floating_point<T>;
-//
-//
-// auto multiply(Numeric auto first, Numeric auto second) {
-//     return first * second;
-// }
-//
-//
-// int main() {
-//     std::println("{}", multiply(3, 4));
-//     std::println("{}", multiply(3, 4.32));
-//
-//
-//     return 0;
-// }
+#include <print>
+
+int main() {
+    return 0;
+}
--- src/simpletron/simpletron.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Peter Sims on 10/12/24.
-//
-
-#include "simpletron.h"
